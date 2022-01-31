11fa47760edddbb2380ee48ed90b8d1893894889
PrabathLonk <cyberdragon.pobo@gmail.com>
2022-01-31 08:41:20 -0600

    Final Rock Paper scissors

--- RockPaperScissorsFinal_Jan27.py
+++ RockPaperScissorsFinal_Jan27.py
@@ -30,80 +30,80 @@ while GameOn==True:
     if int(UserAction)==int(CompAction):
         print ("You tied!")
         response = input("do you want to play again (y for yes, n for no): ")
-        if response =="y":
+        if response =="y" or response =="Y":
             os.system('cls')
             menu()
             CompAction= random.randint(1,3)
             UserAction = input("Type in your choice (1 for rock, 2 for paper, 3 for scissors):")
-        elif response=="n":
+        elif response=="n" or response=="N":
             quit()
     elif int(UserAction)==1 and int(CompAction)==2:
         print("Paper covered your rock! You lose.")
         response = input("do you want to play again (y for yes, n for no): ")
-        if response =="y":
+        if response =="y" or response=="Y":
             os.system('cls')
             menu()
             CompAction= random.randint(1,3)
             UserAction = input("Type in your choice (1 for rock, 2 for paper, 3 for scissors):")
-        elif response=="n":
+        elif response=="n" or response=="N":
             quit()
     elif int(UserAction)==2 and int(CompAction)==3:
         print ("Your paper was cut by scissors. You lose.")
         response = input("do you want to play again (y for yes, n for no): ")
-        if response =="y":
+        if response =="y" or response=="Y":
             os.system('cls')
             menu()
             CompAction= random.randint(1,3)
             UserAction = input("Type in your choice (1 for rock, 2 for paper, 3 for scissors):")
-        elif response=="n":
+        elif response=="n" or response=="N":
             quit()
     elif int(UserAction)==3 and int(CompAction)==1:
         print("Your scissors were crushed by rock. You lose.")
         response = input("do you want to play again (y for yes, n for no): ")
-        if response =="y":
+        if response =="y" or response=="Y":
             os.system('cls')
             menu()
             CompAction= random.randint(1,3)
             UserAction = input("Type in your choice (1 for rock, 2 for paper, 3 for scissors):")
-        elif response=="n":
+        elif response=="n" or response=="N":
             quit()
     elif int(UserAction)==2 and int(CompAction)==1:
         print ("Your paper covers the opposing rock! You win!")
         response = input("do you want to play again (y for yes, n for no): ")
-        if response =="y":
+        if response =="y" or response=="Y":
             os.system('cls')
             menu()
             CompAction= random.randint(1,3)
             UserAction = input("Type in your choice (1 for rock, 2 for paper, 3 for scissors):")
-        elif response=="n":
+        elif response=="n" or response=="N":
             quit()
     elif int(UserAction)==3 and int(CompAction)==2:
         print("Your scissors cut the opposing paper.You win!")
         response = input("do you want to play again (y for yes, n for no): ")
-        if response =="y":
+        if response =="y" or response=="Y":
             os.system('cls')
             menu()
             CompAction= random.randint(1,3)
             UserAction = input("Type in your choice (1 for rock, 2 for paper, 3 for scissors):")
-        elif response=="n":
+        elif response=="n" or response=="N":
             quit()
     elif int(UserAction)==1 and int(CompAction)==3:
         print("Your rock crushed the opposing scissors. You win!")
         response = input("do you want to play again (y for yes, n for no): ")
-        if response =="y":
+        if response =="y" or response=="Y":
             os.system('cls')
             menu()
             CompAction= random.randint(1,3)
             UserAction = input("Type in your choice (1 for rock, 2 for paper, 3 for scissors):")
-        elif response=="n":
+        elif response=="n" or response=="N":
             quit()
     else:
         print("Illegal rock paper scissors move. Try again ")
         response = input("do you want to play again (y for yes, n for no): ")
-        if response =="y":
+        if response =="y" or response=="Y":
             os.system('cls')
             menu()
             CompAction= random.randint(1,3)
             UserAction = input("Type in your choice (1 for rock, 2 for paper, 3 for scissors):")
-        elif response=="n":
+        elif response=="n"or response=="N":
             quit()
