2b48a831952af294417f1cced8c0693144e175a3
PrabathLonk <cyberdragon.pobo@gmail.com>
2022-02-09 14:28:16 -0600

    L

--- ActualWordGameFinal.py
+++ ActualWordGameFinal.py
@@ -1,17 +1,19 @@
-#MariaSuarez 
+#Prabath Girish
 # 02/08/2022
 # Word game with 3 levels: 
-#        1. Fruits    
-#        2. Animals 
-#        3. Computer Parts    
+# 1. Fruits
+# 2.Colors
+# 3.Computer parts    
 # Choice:
 
+import os, random
+os.system('cls')
 #define menu:
 def menu():
     print("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$")
     print("$                                                 $")
     print("$     WELCOME TO THE GUESS THE WORD GAME!!!!!!    $")
-    print("$            Level 1: Foods                       $")
+    print("$            Level 1: Fruits                      $")
     print("$            Level 2: Colors                      $")
     print("$            Level 3: Computer Parts              $")
     print("$                                                 $")
@@ -19,29 +21,65 @@ def menu():
     print("Pick the level by typing 1-3")
 
 menu()
-#Create word lists
-import os, random
-os.system('cls')
-word=""
-guess=""
+#Create word lists and a function to check the level values
+ValueError!=int
+def L():
+    global levelchoice
+    levelchoice=input("what level do you want:")
+    if int(levelchoice)>3 or int(levelchoice)<1:
+        print("L")
+        print("L")
+        print("L")
+        print("L")
+        print("L")
+        print("L")
+        print("L")
+        print("L")
+        print("L")
+        print("L")
+        print("L")
+        print("L")
+        print("L")
+        print("L")
+        print("L")
+        print("L")
+        print("L")
+        print("L")
+        print("L")
+        print("L")
+        print("L")
+        print("L")
+        print("L")
+        print("L")
+        print("L")
+        print("L")
+        print("L")
+        quit()
+# define our word lists:
+fruits=["bananas", "grapes", "waterMelon", 'blueberries', 'apples', "blackberries",
+    "papaya", 'oranges', 'tomatoes', 'mangos', 'kiwis','strawberries', 'monke' ]
+WordBank_Colors=["crimson", "black", "lilac", "white", "orange", "violet", "emerald", "mustard", "sapphire", 'monke']
+WordBank_CompParts=["monitor", "motherboard", "keyboard", "mouse", "speakers", "processor", "battery", "monke"]
+
+L()
+# Create a function to run for each category choice
 def selectWord():
     global word
-    fruits=["bananas", "grapes", "waterMelon", 'blueberries', 'apples', "blackberries",
-    "papaya", 'oranges', 'tomatoes', 'mangos', 'kiwis','strawberries' ]
+    if int(levelchoice)==1:
+        word=random.choice(fruits)
+    elif int(levelchoice)==2:
+        word=random.choice(WordBank_Colors)
+    elif int(levelchoice)==3:
+        word=random.choice(WordBank_CompParts)
 
-    # size= len(fruits)
-    # randy= random.randint(0,size)
-    # print(randy)
-    # word=fruits[randy]
-    # print(word)
-    word=random.choice(fruits)
+
 
 def guessFunction():
     global guess
     check=True
     while check:
         try:
-            guess=input("\nenter a letter to guess the word ")
+            guess=input("\nenter a letter to guess the word: ")
             if guess.isalpha() and len(guess)==1:
                 check=False
             else:
@@ -51,7 +89,7 @@ def guessFunction():
             
 gameOn=True
 tries=0
-letterGuessed=""
+letterGuessed=" "
 selectWord()
 while gameOn:
     
@@ -67,10 +105,17 @@ while gameOn:
             countLetter +=1
         else:
             print("_", end=" ")
-    if tries >6:
-        print("\n Sorry run out chances ")
-        #playGame() ask if they want to play again
+    if tries > len(word):
+        print("\n L, the word was", word)
+        reply=input("do you want to play agian:")
+        if reply=='y':
+            os.system('cls')
+            menu()
+            tries=0
+            L()
+            selectWord()
+            gameOn=True
     if countLetter == len(word):
-        print ("\nyou guessed! ")
+        print ("\nyou guessed the word! ")
         #Calculate score
         #playGame()
