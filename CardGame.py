3ab3a5c0dbaaeec73c0b4fd534205b137254e89c
PrabathLonk <cyberdragon.pobo@gmail.com>
2022-02-19 10:22:43 -0600

    War Game PT1

--- CardGame.py
+++ CardGame.py
@@ -1,29 +1,23 @@
 #Prabath Girish
 #2/16/22
-
+#Previous Draft:
+# Create the "i" function to form the list
+# numbercards=[]
+# for i in range (2,11):
+#     numbercards.append(i)
+#     numbercards[i-2]=str(numbercards[1-2])
+# numbercards.insert(0,'A')
 import os,random
 os.system('cls')
 
-# Create the "i" function to form the list
-numbercards=[]
-for i in range (2,11):
-    numbercards.append(i)
-    numbercards[i-2]=str(numbercards[1-2])
-numbercards.insert(0,'A')
-suits=['s','h','d','c']
-royals=['J','Q',"K"]
-# We have to make a final deck that compiles all the cards in a deck
-fulldeck=[]
-# define a function to create this deck
-def deck(cardnumber):
-    global suits
-    global fulldeck
-    global fulldeck
-    for i in range(len(suits)):
-        suits[i]=str(numbercards[cardnumber])+str(suits[i])
-    fulldeck.extend(suits)
-#Exapnding the full deck to include all the suits and royals
-for l in range(0,8):
-    deck(l)
-fulldeck.extend(royals) #Extend to include the royals
-print("The deck is:\n"+str(fulldeck))
+#Define your deck and the possible suits, while creating a function for the full deck 
+suits=['of spades','of hearts','of diamonds','of clubs']
+deck=['Ace',2,3,4,5,6,7,8,9,10,"Jack",'Queen','King']
+mainDeck=[]
+# Create a for function that adds a suit for every number/royal to create the deck of 52
+for num in deck:
+    for SuitChoice in suits:
+        mainDeck.append(str(num)+' '+SuitChoice)
+
+print("MainDeck:\n", mainDeck)
+userCard1=random.choice(mainDeck)
