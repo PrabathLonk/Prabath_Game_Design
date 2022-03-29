4e6f9ab8ae6b8a246db01bd44e0dbc98e6f551bc
PrabathLonk <cyberdragon.pobo@gmail.com>
2022-03-29 18:40:31 -0500

    game update

--- FinalCircleEatSquareGame/CircleEatSquareFinished.py
+++ FinalCircleEatSquareGame/CircleEatSquareFinished.py
@@ -33,6 +33,7 @@ hb=30
 MAIN=True
 INST=False
 SETT=False
+GAME=False
 LEV_I=False
 #List f messages
 MenuList=['Instructions','Settings', "Play Game","Exit",'Scoreboard']
@@ -134,7 +135,166 @@ def SettMenu(Mlist):
         pygame.draw.rect(screen,sqM_color, squareSet )
         squareSet.y +=50
         txtS+=50
+MAX=10
+jumpCount=MAX
+JUMP=False
+HitX=xc-15
+HitY=yc-15
+CRadius=15
+HitLenght=CRadius*2
+HitWidth=CRadius*2
+hitbox=pygame.Rect(HitX,HitY,HitWidth,HitLenght)
+move=5
+ColorCheck=False
+def ActualGame():
+    global move
+    global check
+    global screen
+    global square
+    global hitbox
+    global c_color
+    global background
+    global Hit_color
+    global s_color
+    global jumpCount
+    global JUMP
+    global CRadius
+    global MAX
+    global HitLenght
+    global HitWidth
+    global ChangeColor
+    global HitX
+    global HitY
+    global ColorCheck
+    move=5 #pixels
+    # Declare constant variables, list, dictionaries
+    #Capitalize to clarify constants
+    #Screen size
+    WIDTH=700
+    HEIGHT=700
+    check=True #<-- For while loop
+    #Square position and size
+    xs=20
+    ys=20
+    wbox=30
+    hbox=30
+    # Circle radius
+    CRadius=15
+    #dimesions for the hitbox
+    HitLenght=CRadius*2
+    HitWidth=CRadius*2
+    # Circle random start point (making sure the circle doesn't appear partly offscreen)
+    xc=random.randint(CRadius,WIDTH-(2*CRadius))
+    yc=random.randint(CRadius,HEIGHT-(2*CRadius))
+    HitX=xc-15
+    HitY=yc-15
+    # create the objects
+    #Our screen:
+    screen=pygame.display.set_mode((WIDTH,HEIGHT))
+    pygame.display.set_caption("Circle Eats Square")
+    # "Rect" stands for the rectangle shape type
+    #The measurments go in x position, y position, width, height 
+    square=pygame.Rect(xs,ys,wbox,hbox)
 
+    #Dimesions of the circle hitbox
+    hitbox=pygame.Rect(HitX,HitY,HitWidth,HitLenght)
+
+
+    #Create the screen
+    pygame.display.set_mode((WIDTH,HEIGHT))
+
+    #Define our colors in a dictionary
+    colors={'white':[255,255,255], 'red':[255,0,0], 'orange':[255,85,0], 'purple':[48,25,52,],'aqua':[102,193,255], 'pink': [200,3,75], 'black':[0,0,0], 'navy':[5,31,64]}
+
+    #Getting a random color:
+    RandColor=random.choice(list(colors))
+    #Call colors to get colors for our screen and shapes
+    background=colors.get('white')
+    # s_color=colors.get('navy') <--- Previous square color
+    c_color=colors.get('navy')
+    Hit_color=colors.get('white')
+    # Creating a color check to make sure our colors are all different:
+    def ChangeColor():
+        global RandColor
+        ColorCheck=True
+        while ColorCheck==True:
+            RandColor=random.choice(list(colors)) #<--- Getting a random color for the square
+            if colors.get(RandColor)==background:
+                RandColor=random.choice(list(colors))
+            else:
+                ColorCheck=False
+
+    ChangeColor()
+    s_color=colors.get(RandColor) #<--- Getting a random color for the square
+
+
+    #make a function for our game
+    while check:
+        #Fill the screen and draw the shapes (for testing)
+        screen.fill(background)
+        #Checking for events in the pygame and allow for key inputs
+        #For keys use K_(key value)
+        #arrows for circle and wasd for squares
+        for case in pygame.event.get():
+            if case.type==pygame.QUIT:
+                check=False
+        keys=pygame.key.get_pressed() #<-- To check if a key gets pressed (classified as a list), the 'and move' part has to do with creating boundries
+        # Movements for the square
+        if keys[pygame.K_a] and square.x>=move:
+            square.x-=move #subtract
+        if keys[pygame.K_d] and square.x<=WIDTH-(wbox+move):
+            square.x+=move
+        #Jumping
+        if JUMP==False:
+            if keys[pygame.K_w] and square.y>=move:
+                square.y-=move
+            if keys[pygame.K_s] and square.y<=HEIGHT-(hbox+move):
+                square.y+=move
+            if keys[pygame.K_SPACE]:
+                JUMP=True
+        else:
+            if jumpCount>=-MAX:
+                square.y-=jumpCount*abs(jumpCount)/2
+                jumpCount-=1
+            else:
+                jumpCount=MAX
+                JUMP=False
+        # Circle Movements
+        if keys[pygame.K_LEFT] and xc>=move+CRadius:
+            xc-=move #subtract
+            hitbox.x-=move
+        if keys[pygame.K_RIGHT] and xc<=WIDTH-(CRadius+move):
+            xc+=move
+            hitbox.x+=move
+        if keys[pygame.K_UP] and yc>=move+CRadius:
+            yc-=move
+            hitbox.y-=move
+        if keys[pygame.K_DOWN] and yc<=HEIGHT-(CRadius+move):
+            yc+=move
+            hitbox.y+=move
+        #Making the collision
+        checkCollide= square.colliderect(hitbox)
+        if checkCollide==True:
+            square.x=random.randint(wbox,WIDTH-wbox)
+            square.y=random.randint(hbox,HEIGHT-hbox)
+            CRadius+=move
+            HitWidth+=move
+            HitLenght+=move
+            ColorCheck=True
+            ChangeColor()
+            s_color=colors.get(RandColor)
+
+        pygame.draw.rect(screen,s_color,square)
+        pygame.draw.rect(screen,Hit_color,hitbox)
+        pygame.draw.circle(screen,c_color,(xc,yc),CRadius)
+        if CRadius==80:
+            print("The circle player has won the game! Congrats! Switch roles as see if you can beat that time! ")
+            check=False
+
+        #Display the screen and shapes via updating (for testing)
+        pygame.display.update()
+        #Add a delay so that we can see our shapes (for testing)
+        pygame.time.delay(10)
 #sq_color=colors.get('navy')
 #Making a rand c f the square
 changeColor()
@@ -152,8 +312,10 @@ while check:
     if SETT:
         TitleMenu("SETTINGS")
         SettMenu(SettingList)
-        BackButton=MENU_FONT.render("BACK",1,(0,0,0))
-        screen.blit(BackButton,(250,500))
+        BackButton=MENU_FONT.render("BACK (Press ESC)",1,(0,0,0))
+        screen.blit(BackButton,(200,500))
+    if GAME:
+        ActualGame()
     for case in pygame.event.get():
         if case.type==pygame.QUIT:
             check=False
@@ -173,20 +335,34 @@ while check:
             instructions4=INSTRUCTION_FONT.render("square, the circle is controlled by Player 2 with the arrow",1,(0,0,255))
             instructions4=INSTRUCTION_FONT.render("keys. The square can hit space to get a vertical jump.",1,(0,0,255))
             instructions5=INSTRUCTION_FONT.render("Once a circle reaches a certain size, the circle player wins",1,(0,0,255))
-            instructions6=INSTRUCTION_FONT.render("Try timing youself to trya get your best time as the circle",1,(0,0,255))
-            BackButton=MENU_FONT.render("BACK",1,(0,0,0))
+            instructions6=INSTRUCTION_FONT.render("Try timing youself to try and get your best time as the circle",1,(0,0,255))
+            BackButton=MENU_FONT.render("BACK (Press ESC)",1,(0,0,0))
             screen.blit(instructions,(20,200))
             screen.blit(instructions2,(20,230))
             screen.blit(instructions3,(20,260))
             screen.blit(instructions4,(15,290))
             screen.blit(instructions5,(20,320))
             screen.blit(instructions6,(20,350))
-            screen.blit(BackButton,(250,500))
+            screen.blit(BackButton,(200,500))
+            if ((mouse_pos[0] >200 and mouse_pos[0] <540) and (mouse_pos[1] >500 and mouse_pos[1] <540)):
+                screen.fill(background)
+                INST=False
+                MAIN=True
+                TitleMenu("MENU")
+                MainMenu(MenuList)
         elif ((mouse_pos[0] >20 and mouse_pos[0] <80) and (mouse_pos[1] >300 and mouse_pos[1] <340)) or SETT:
             MAIN=False
             screen.fill(background)
             SETT=True
-            
+            if ((mouse_pos[0] >200 and mouse_pos[0] <540) and (mouse_pos[1] >500 and mouse_pos[1] <540)):
+                screen.fill(background)
+                SETT=False
+                MAIN=True
+                TitleMenu("MENU")
+                MainMenu(MenuList)
+        elif ((mouse_pos[0] >20 and mouse_pos[0] <80) and (mouse_pos[1] >350 and mouse_pos[1] <390)) or GAME:
+            screen.fill(background)
+            ActualGame()
             
 
 
