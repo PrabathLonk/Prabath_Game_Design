ada7365f90f7b7a60c411fad129e193ef49e0518
PrabathLonk <cyberdragon.pobo@gmail.com>
2022-04-12 20:02:20 -0500

    update?

--- FinalProjectResources/ImagesTutorial.py
+++ FinalProjectResources/ImagesTutorial.py
@@ -15,13 +15,18 @@ bg2=pygame.image.load('FinalProjectResources\Images\Sunset.jpg')
 bg3=pygame.image.load('FinalProjectResources\Images\Handshake.jpg')
 bg4=pygame.image.load('FinalProjectResources\Images\GrassyBackground.jpg')
 bg5=pygame.image.load('FinalProjectResources\Images\Grassland.jpg')
+rip=pygame.image.load('FinalProjectResources\Images\Rip2.png')
 bgList=[bg,bg2,bg3,bg4,bg5]
 char = pygame.image.load('FinalProjectResources\Images\standing.png')
 thwomp= pygame.image.load('FinalProjectResources\Images\Thwomp.png')
 x = 50
 y = 400
+hitX=50
+hitY=400
 width = 40
 height = 60
+hitbox=pygame.Rect(hitX,hitY,width+20,height+10)
+ThwompBox=pygame.Rect(250,140,150,160)
 vel = 5
 MAX=-10
 
@@ -37,25 +42,30 @@ walkCount = 0
 def redrawGameWindow():
     global walkCount
     
-    win.blit(bg, (0,0))  
+    win.blit(bg, (0,0)) 
+    pygame.draw.rect(win,(255,255,255),ThwompBox) 
     win.blit(thwomp,(250,140))
     if walkCount + 1 >= 27:
         walkCount = 0
         
-    if left:  
+    if left: 
+        pygame.draw.rect(win,(255,255,255),hitbox) 
         win.blit(walkLeft[walkCount//3], (x,y))
         walkCount += 1 
-        if ((x >140 and x < 290) and (y >250 and y <370)):  
-            win.blit(char,(x,y-5)) 
-            win.blit(char,(x,y-5)) 
-            win.blit(char,(x,y-5))
-            print(x+"and"+y)
+        # if ((x >140 and x < 290) and (y >250 and y <370)):  
+        #     # right=False
+        #     if y!=400:
+        #         y=y+10
+        #     print(x+"and"+y)
+        #     # left=False
             
  
     elif right:
+        pygame.draw.rect(win,(255,255,255),hitbox)
         win.blit(walkRight[walkCount//3], (x,y))
         walkCount += 1
     else:
+        pygame.draw.rect(win,(255,255,255),hitbox)
         win.blit(char, (x, y))
         walkCount = 0
         
@@ -76,20 +86,54 @@ while run:
     
     if keys[pygame.K_LEFT] and x > vel: 
         x -= vel
+        hitbox.x=x
         left = True
         right = False
+        checkCollide= pygame.Rect.colliderect(ThwompBox,hitbox)
+        if checkCollide:
+            isJump=False
+            y=400
+            hitbox.y=y
+            win.blit(rip,(x,y))
+            print("Oof you died. Try again later")
+            run=False
+
+
+
+        pygame.display.update()
 
     elif keys[pygame.K_RIGHT] and x < 500 - vel - width:  
         x += vel
+        hitbox.x=x
         left = False
         right = True
+        checkCollide= pygame.Rect.colliderect(ThwompBox,hitbox)
+        if checkCollide:
+            isJump=False
+            y=400
+            hitbox.y=y
+            win.blit(rip,(x,y))
+            print("Oof you died. Try again later")
+            run=False
+
+
+        pygame.display.update()
         
     else: 
         left = False
         right = False
         walkCount = 0
+        checkCollide= pygame.Rect.colliderect(ThwompBox,hitbox)
+        if checkCollide:
+            isJump=False
+            y=400
+            hitbox.y=y
+            win.blit(rip,(x,y))
+            print("Oof you died. Try again later")
+            run=False
 
-    if x== 480-80:
+
+    if x== 400:
         bg=random.choice(list(bgList))
         y=400
         x=0
@@ -108,13 +152,25 @@ while run:
             left = False
             right = False
             walkCount = 0
+            
     else:
         if jumpCount >= MAX:
             y -= (jumpCount * abs(jumpCount)) * 0.5
+            hitbox.y=y
             jumpCount -= 1
+            if checkCollide:
+                isJump=False
+                y=400
+                hitbox.y=y
+                
+                win.blit(rip,(x,y))
+                print("Oof you died. Try again later")
+                run=False
+
         else: 
             jumpCount = 10
-            isJump = False
+            isJump = False    
+
 
     redrawGameWindow() 
     
